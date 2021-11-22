4bad0441a4ffe51f1520753e9e3be458498a27c7
Arun Tekwani <atekwani@deqode.com>
2021-11-22 17:17:02 +0530

    add create product api.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,28 @@
 class ProductsController < SecuredController
-	# before_action :authorize_request, only: [:index]
+	skip_before_action :authorize_request, only: [:index]
 
 	def index
 		products = Product.all
-		render json: {products: products, :each_serializer => ProductSerializer}
+		if products.present?
+			render json: {products: ActiveModel::ArraySerializer.new(products, each_serializer: ProductSerializer)}
+		else
+			render json: {errors: ["doesn't have any product."]}
+		end
+	end
+
+	def create
+		product = Product.new(product_params)
+		if product.save
+			render json: product, root: 'product', serializer: ProductSerializer
+		else
+			render json: {errors: product.errors}
+		end
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name, :price)
 	end
 
 end
